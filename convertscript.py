9254ebd31a7552653d221c94849ffe4ab63a230f
ashkanpakzad <ashpakzad@gmail.com>
2020-09-09 10:38:45 +0100

    converted images and annotations to classes and added conversion script

---
+++ convertscript.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+import datetime
+import json
+import os
+import re
+import fnmatch
+from PIL import Image
+import numpy as np
+from pycococreatortools import pycococreatortools
+from pathlib import Path
+from pycocotools import mask
+from medicalpycoco.cocoobjects import COCOimage
+
+datasetname = 'dataset'  # savename
+ROOT_DIR = 'train'
+IMAGE_DIR = os.path.join(ROOT_DIR, "img")
+ANNOTATION_DIR = os.path.join(ROOT_DIR, "mask")
+
+INFO = {
+    "description": "description",
+    "url": "N/A",
+    "version": "0.1.0",
+    "year": 2020,
+    "contributor": "ashkanpakzad",
+    "date_created": datetime.datetime.utcnow().isoformat(' ')
+}
+
+LICENSES = [
+    {
+        "id": 1,
+        "name": "Attribution-NonCommercial-ShareAlike License",
+        "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/"
+    }
+]
+
+CATEGORIES = [
+    {
+        'id': 0,
+        'name': 'laptop',
+        'supercategory': 'laptop',
+    },
+]
+
+
+def filter_for_jpeg(root, files):
+    file_types = ['*.jpeg', '*.jpg']
+    file_types = r'|'.join([fnmatch.translate(x) for x in file_types])
+    files = [os.path.join(root, f) for f in files]
+    files = [f for f in files if re.match(file_types, f)]
+
+    return files
+
+
+def filter_for_annotations(root, files, image_filename):
+    file_types = ['*.png']
+    file_types = r'|'.join([fnmatch.translate(x) for x in file_types])
+    basename_no_extension = os.path.splitext(os.path.basename(image_filename))[0]
+    file_name_prefix = basename_no_extension + '.*'
+    files = [os.path.join(root, f) for f in files]
+    files = [f for f in files if re.match(file_types, f)]
+    files = [f for f in files if re.match(file_name_prefix, os.path.splitext(os.path.basename(f))[0])]
+
+    return files
+
+
+def main():
+    coco_output = {
+        "info": INFO,
+        "licenses": LICENSES,
+        "categories": CATEGORIES,
+        "images": [],
+        "annotations": []
+    }
+
+    segmentation_id = 1
+
+    # filter for jpeg images
+    for root, _, files in os.walk(IMAGE_DIR):
+        image_files = filter_for_jpeg(root, files)
+
+        # go through each image
+        for image_filename in image_files:
+            image_id = int(Path(image_filename).stem)
+            image_info = COCOimage(image_id, image_filename).todict()
+            coco_output["images"].append(image_info)
+
+            # filter for associated png annotations
+            for root, _, files in os.walk(ANNOTATION_DIR):
+                annotation_files = filter_for_annotations(root, files, image_filename)
+
+                # go through each associated annotation
+                for annotation_filename in annotation_files:
+
+                    print(annotation_filename)
+                    class_id = [x['id'] for x in CATEGORIES if x['name'] in annotation_filename][0]
+
+                    category_info = {'id': class_id, 'is_crowd': 'crowd' in image_filename}
+                    binary_mask = np.array(Image.open(annotation_filename)).astype(np.uint8)
+
+                    annotation_info = COCOann(segmentation_id, image_id, category_info, binary_mask,
+                                              image_size=[image_info['width'], image_info['height']]).todict()
+
+                    if annotation_info is not None:
+                        coco_output["annotations"].append(annotation_info)
+
+                    segmentation_id = segmentation_id + 1
+
+    with open(f'{ROOT_DIR}/{datasetname}.json', 'w') as output_json_file:
+        json.dump(coco_output, output_json_file)
+
+
+if __name__ == "__main__":
+    main()
---
+++ medicalpycoco/cocoobjects.py
@@ -0,0 +1,59 @@
+import datetime
+import numpy as np
+from PIL import Image
+from pathlib import Path
+from pycocotools import mask
+from .medicalpycocotools import binary_mask_to_rle, resize_binary_mask
+
+class COCOimage:
+    def __init__(self, image_id, image_path,
+                      date_captured=datetime.datetime.utcnow().isoformat(' '),
+                      license_id=1, coco_url="", flickr_url=""):
+        image = Image.open(image_path)
+        self.file_name = Path(image_path).name
+        [self.width, self.height] = image.size
+        self.id = image_id
+
+        self.date_captured = date_captured
+        self.license = license_id
+        self.coco_url = coco_url
+        self.flickr_url = flickr_url
+
+    def todict(self):
+        return self.__dict__
+
+class COCOann:
+    def __init__(self, annotation_id, image_id, category_info, binary_mask, image_size=None, bounding_box=None):
+
+        if image_size is not None:
+            binary_mask = resize_binary_mask(binary_mask, image_size)
+
+        binary_mask_encoded = mask.encode(np.asfortranarray(binary_mask.astype(np.uint8)))
+        area = mask.area(binary_mask_encoded)
+
+        if bounding_box is None:
+            bounding_box = mask.toBbox(binary_mask_encoded)
+
+        if category_info["is_crowd"]:
+            is_crowd = 1
+        else :
+            is_crowd = 0
+
+        segmentation = binary_mask_to_rle(binary_mask)
+
+        self.id = annotation_id
+        self.image_id= image_id
+        self.category_id= category_info["id"]
+        self.iscrowd= is_crowd
+        self.area= area.tolist()
+        self.bbox= bounding_box.tolist()
+        self.segmentation= segmentation
+        self.width= binary_mask.shape[1]
+        self.height= binary_mask.shape[0]
+
+    def todict(self):
+        if self.area < 1 or not self.segmentation:
+            # no annotation
+            return None
+        else:
+            return self.__dict__
