c4c546f44d2f8c6cb9555502e52f8e033badda1d
ashkanpakzad <ashpakzad@gmail.com>
2020-09-16 21:35:45 +0100

    uses merge flag

--- preconversion.py
+++ preconversion.py
@@ -3,17 +3,18 @@ import os
 from medicalpycoco.medicalconverttools import preprocess
 
 # set up paths
-ROOT_DIR = 'Task06_Lung/val'
+ROOT_DIR = 'Task08_HepaticVessel/test'
 IMAGE_VOL_DIR = os.path.join(ROOT_DIR, "volume")
 ANNOTATION_VOL_DIR = os.path.join(ROOT_DIR, "label")
 
-classes = {'1': 'tumour'}
+classes = {'1': 'vessel', '2': 'tumour'}
 roi = [
-    [40, 460], [60, 410], [50, -10]
+    [100, 420], [60, 440], [2, -2]
 ]
+merge = True # merge multiple instances in a single slice into one.
 
 def main():
-    img_dir, ann_dir = preprocess(classes, IMAGE_VOL_DIR, ANNOTATION_VOL_DIR, ROOT_DIR, roi=roi, reorient=False)
+    img_dir, ann_dir = preprocess(classes, IMAGE_VOL_DIR, ANNOTATION_VOL_DIR, ROOT_DIR, roi=roi, merge=merge, reorient=True)
     print(img_dir)
     print(ann_dir)
 if __name__ == "__main__":
